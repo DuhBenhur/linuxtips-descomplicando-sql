87de13847d298f0894c04d4613be4f3a3a708b48
eduardo.benhur@gmaiil.com <eduardo.benhur@gmaiil.com>
2022-12-15 23:41:29 +0000

    Exercicios

--- Bora_praticar.sql
+++ Bora_praticar.sql
@@ -107,4 +107,41 @@ SELECT
 
 -- COMMAND ----------
 
+-- 06 Lista de pedidos feitosem dezembro de 2017 e entregues  com atraso
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  YEAR(dtPedido) = 2017
+  AND MONTH(dtPedido) = 12
+  AND descSituacao = 'delivered'
+  AND date(dtEntregue) > date(dtEstimativaEntrega)
+
+-- COMMAND ----------
+
+--08 Lista de pedidos com 2 ou mais parcelas menores que R$20,00
+SELECT * ,
+ROUND(vlPagamento / nrPacelas,2) AS vlParcela
+FROM silver_olist.pagamento_pedido
+WHERE nrPacelas >= 2
+AND vlPagamento / nrPacelas < 20
+
+-- COMMAND ----------
+
+SELECT
+  *,
+  vlPreco + vlFrete as vlTotal,
+  Round(vlFrete / (vlPreco + vlFrete), 2) AS pctFrete,
+  CASE
+    WHEN Round(vlFrete / (vlPreco + vlFrete), 2) <= 0.1 THEN '10%'
+    WHEN Round(vlFrete / (vlPreco + vlFrete), 2) <= 0.25 THEN '10% a 25%'
+    WHEN Round(vlFrete / (vlPreco + vlFrete), 2) <= 0.5 THEN '25% a 50%'
+    ELSE '+50%'
+  END AS descFretePct
+FROM
+  silver_olist.item_pedido
+
+-- COMMAND ----------
+
 
